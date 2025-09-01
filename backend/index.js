36cae36ca9ac3c867331be1839cbf58b3a6c772f
PRIYANSHU SONI <122470603+priyanshu08soni@users.noreply.github.com>
2025-09-01 17:55:58 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,32 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
-const authRouter = require('./controllers/authController'); // Make sure this points to the correct file
-const quizRouter = require('./controllers/quizController');
+const authRouter = require("./controllers/authController"); // Make sure this points to the correct file
+const quizRouter = require("./controllers/quizController");
 const app = express();
 
-mongoose.set('strictQuery', false)
-mongoose.connect(process.env.MONGO_URL, () => console.log('DB is successfully connected'))
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.MONGO_URL, () =>
+  console.log("DB is successfully connected")
+);
 
 // Use middleware
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.use('/auth', authRouter); // Ensure this correctly routes to the AuthRouter !
-app.use('/quizzes', quizRouter);
+app.use("/auth", authRouter); // Ensure this correctly routes to the AuthRouter !
+app.use("/quizzes", quizRouter);
 
 // Start server
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on ${process.env.PORT}`);
+  console.log(`Server is running on ${process.env.PORT}`);
 });
