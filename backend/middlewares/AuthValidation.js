af875f1273d3cfc41a87063414113aafbad05f42
PRIYANSHU SONI <122470603+priyanshu08soni@users.noreply.github.com>
2025-09-04 20:32:12 +0530

    commit

--- backend/middlewares/AuthValidation.js
+++ backend/middlewares/AuthValidation.js
@@ -2,17 +2,19 @@ const Joi = require('joi');
 
 const signupValidation = (req, res, next) => {
     const schema = Joi.object({
-        name: Joi.string().min(3).max(100).required(),
-        email: Joi.string().email().required(),
-        password: Joi.string().min(4).max(100).required()
+      name: Joi.string().min(3).max(100).required(),
+      username: Joi.string().alphanum().min(3).max(30).required(), // 👈 added
+      email: Joi.string().email().required(),
+      password: Joi.string().min(4).max(100).required()
     });
+  
     const { error } = schema.validate(req.body);
     if (error) {
-        return res.status(400)
-            .json({ message: "Bad request", error })
+      return res.status(400).json({ message: "Bad request", error });
     }
     next();
-}
+  };
+  
 const loginValidation = (req, res, next) => {
     const schema = Joi.object({
         email: Joi.string().email().required(),
